8241a1169e8098dfce57dd2150f751802ad00a9f
SpinnyCow27 <matias.04@hotmail.es>
2024-06-30 18:59:15 -0400

    Arreglado el header, añadido quienes somos, añadido destacados

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#Destacados .card-title').click(function() {
+        $(this).closest('.card').toggle();
+        if ($('.card:hidden').length > 0) {
+            $('#showAllCards').show();
+        } else {
+            $('#showAllCards').hide();
+        }
+    });
+    
+    $('#showAllCards').click(function() {
+        $('.card:hidden').toggle();
+        $(this).hide();
+    });
+
+    if ($('.card:hidden').length === 0) {
+        $('#showAllCards').hide();
+    }
+});
